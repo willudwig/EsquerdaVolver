bb287c5f00170f3e527f9c104dfb6cfada3a018f
William Ludwig <acesso.william@gmail.com>
2022-02-24 09:59:08 -0300

    Add project files.

---
+++ EsquerdaVolverConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace EsquerdaVolverConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movimentos = "dde".ToUpper();
+            char[] mov = movimentos.ToCharArray();
+            char orientacao = 'N';
+
+            MudarDirecao(ref orientacao, mov);
+            Console.WriteLine("\n" + orientacao);
+
+            Console.ReadKey();
+        }
+
+        static void MudarDirecao(ref char orient, char[] mov)
+        {
+            for (int i = 0; i < mov.Length; i++)
+            {
+                switch (mov[i])
+                {
+                    case 'E':
+                        switch (orient)
+                        {
+                            case 'N':
+                                orient = 'O';
+                                break;
+                            case 'S':
+                                orient = 'L';
+                                break;
+
+                            case 'L':
+                                orient = 'N';
+                                break;
+
+                            case 'O':
+                                orient = 'S';
+                                break;
+                        }
+                        break;
+
+                    case 'D':
+                        switch (orient)
+                        {
+                            case 'N':
+                                orient = 'L';
+                                break;
+                            case 'S':
+                                orient = 'O';
+                                break;
+
+                            case 'L':
+                                orient = 'S';
+                                break;
+
+                            case 'O':
+                                orient = 'N';
+                                break;
+                        }
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
